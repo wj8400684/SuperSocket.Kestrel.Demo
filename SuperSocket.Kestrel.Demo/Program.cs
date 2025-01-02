6838276df15e95c5fc41e6de19b11ba404cf9f2a
wujun <8400684@qq.com>
2025-01-02 08:51:33 +0800

    ini

--- SuperSocket.Kestrel.Demo/Program.cs
+++ SuperSocket.Kestrel.Demo/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Hosting;
+using SuperSocket.Kestrel;
+using SuperSocket.ProtoBase;
+using SuperSocket.Server.Host;
+
+await SuperSocketHostBuilder.Create<TextPackageInfo, LinePipelineFilter>()
+    .UsePackageHandler(packageHandler: async (session, package) =>
+    {
+        await ValueTask.CompletedTask;
+    })
+    .UseClearIdleSession()
+    .UseKestrelPipeConnection()
+    .Build()
+    .RunAsync();
